8f7ae080224d421608c73ea2932555c480cbba72
Laveesha Khatri <119026537+Laveesha02@users.noreply.github.com>
2026-09-22 14:20:13 +0530

    Update App.js

--- starter/frontend/src/App.js
+++ starter/frontend/src/App.js
@@ -1,8 +1,12 @@
+import "./App.css";
+
 function App() {
   return (
-    <div>
-      <h1>Movie Picture Pipeline</h1>
-      <p>Welcome to the Movie Picture application.</p>
+    <div className="App">
+      <header className="App-header">
+        <h1>Movie Picture Pipeline</h1>
+        <p>Welcome to the Movie Picture application.</p>
+      </header>
     </div>
   );
 }
