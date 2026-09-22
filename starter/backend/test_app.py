5413af19cc9f7bc7ae32ec2702fcfad58a5c0d5b
Laveesha Khatri <119026537+Laveesha02@users.noreply.github.com>
2026-09-22 14:08:47 +0530

    Create test_app.py

---
+++ starter/backend/test_app.py
@@ -0,0 +1,22 @@
+import pytest
+
+from app import create_app
+
+
+@pytest.fixture
+def client():
+    app = create_app()
+    app.config["TESTING"] = True
+
+    with app.test_client() as client:
+        yield client
+
+
+def test_app_is_created(client):
+    response = client.get("/api")
+    assert response.status_code in [200, 404]
+
+
+def test_app_is_testing(client):
+    response = client.get("/api")
+    assert response.status_code in [200, 404]
